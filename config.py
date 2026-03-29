77f915c2cce47f09bb302ee241b151ce93d0ad09
Arpita Landge <landgearpita25@gmail.com>
2026-03-29 21:29:49 +0530

    Updated project before pull

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'tasks.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
